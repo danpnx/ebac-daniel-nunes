a78656f58516a5fcde51f30fc2cbf7b47176989c
Daniel Augusto <danielpn23@outlook.com>
2023-01-10 18:01:01 -0300

    Adiciona diretório mod05 com a tarefa do módulo 04

---
+++ mod05/exercicio-modulo-4/src/main/java/com/org/Main.java
@@ -0,0 +1,9 @@
+package br.art.ebac;
+
+public class Main {
+    public static void main(String[] args) {
+        String message = args[0];
+
+        System.out.println("Hello, " + message);
+    }
+}
